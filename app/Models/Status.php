d6af8e376ab4447e8f3a66aec6b1496f499ccf0a
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-12-28 02:30:07 +1000

    model of Status

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    //
+    protected $fillable = [
+        'name', // 'available' or 'booked'
+    ];
+
+    public function resources()
+    {
+        return $this->hasMany(Resource::class);
+    }
 }
