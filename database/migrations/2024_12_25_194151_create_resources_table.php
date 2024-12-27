646b7c21f72fe3cd96a68ad56412d90565ff1389
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-12-28 02:23:56 +1000

    Create migrations of bookings and resources

--- database/migrations/2024_12_25_194151_create_resources_table.php
+++ database/migrations/2024_12_25_194151_create_resources_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('resources', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('capacity');
+            $table->foreignId('status_id')->constrained();
             $table->timestamps();
         });
     }
--- database/migrations/2024_12_25_194159_create_bookings_table.php
+++ database/migrations/2024_12_25_194159_create_bookings_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('resource_id')->constrained();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
             $table->timestamps();
         });
     }
