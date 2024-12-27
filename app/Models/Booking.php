4d94f903d8d644f782854aaaaa5b8022cc879cfd
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-12-28 02:28:53 +1000

    model of Booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'resource_id',
+        'start_date',
+        'end_date',
+    ];
+
+        public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function resource()
+    {
+        return $this->belongsTo(Resource::class);
+    }
 }
