197923176697b5ba5dbe4fea09283433e068e4ca
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-12-28 02:27:22 +1000

    Model of Resource

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resource extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'capacity',
+        'status_id',
+    ];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
 }
