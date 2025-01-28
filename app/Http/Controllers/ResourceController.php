360d2f43fe8c88e5d319d953ac46017ef94d61bb
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2025-01-29 04:21:15 +1000

    Update ResourceController

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -14,28 +14,28 @@ class ResourceController extends Controller
         return response()->json($resources);
     }
 
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'capacity' => 'required|integer',
-            'status_id' => 'required|integer',
-        ]);
+    // public function store(Request $request)
+    // {
+    //     $request->validate([
+    //         'name' => 'required|string|max:255',
+    //         'capacity' => 'required|integer',
+    //         'status_id' => 'required|integer',
+    //     ]);
 
-        $resource = Resource::create($request->all());
-        return response()->json($resource, 201);
-    }
+    //     $resource = Resource::create($request->all());
+    //     return response()->json($resource, 201);
+    // }
 
-    public function update(Request $request, $id)
-    {
-        $resource = Resource::findOrFail($id);
-        $resource->update($request->all());
-        return response()->json($resource);
-    }
+    // public function update(Request $request, $id)
+    // {
+    //     $resource = Resource::findOrFail($id);
+    //     $resource->update($request->all());
+    //     return response()->json($resource);
+    // }
 
-    public function destroy($id)
-    {
-        Resource::destroy($id);
-        return response()->json(['message' => 'Ресурс удален']);
-    }
+    // public function destroy($id)
+    // {
+    //     Resource::destroy($id);
+    //     return response()->json(['message' => 'Ресурс удален']);
+    // }
 }
